050104028e3edbce5ea5a80b369503ba9c0299fd
Mary Goodhart <mary.loves.pry@gmail.com>
2019-03-20 13:07:04 -0600

    Adds setup for index.js file

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment]
+const database = require('knex')(configuration);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Songs';
