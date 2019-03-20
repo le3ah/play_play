5d5e00a76fb2da45d30c78c9711492d5a246726d
Mary Goodhart <mary.loves.pry@gmail.com>
2019-03-20 13:25:51 -0600

    Adds favorites api with passing test

--- index.js
+++ index.js
@@ -11,7 +11,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Songs';
 
-
+app.get('/api/v1/favorites', (request, response) => {
+  database('favorites').select()
+    .then((favorites) => {
+      response.status(200).json(favorites);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
 
 module.exports = {
   app: app,
--- test/routes.spec.js
+++ test/routes.spec.js
@@ -14,8 +14,17 @@ describe('Favorite Routes', () => {
     .get('/api/v1/favorites')
     .end((err, response) => {
       response.should.have.status(200);
-      response.should.be.html;
-      response.res.text.should.equal('whatever');
+      response.should.be.json;
+      response.body.should.be.a('array');
+      response.body.length.should.equal(1);
+      response.body[0].should.have.property('name');
+      response.body[0].name.should.equal('Crazy');
+      response.body[0].should.have.property('artist_name');
+      response.body[0].artist_name.should.equal('Patsy Cline');
+      response.body[0].should.have.property('genre');
+      response.body[0].genre.should.equal('Country');
+      response.body[0].should.have.property('rating');
+      response.body[0].rating.should.equal(98);
       done();
     })
   })
