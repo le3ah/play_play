e2ba35687113dde05b63c5c027e7790ca7bef411
Mary Goodhart <mary.loves.pry@gmail.com>
2019-03-20 15:29:25 -0600

    Adds seeding for joins table

--- db/seeds/dev/favorites.js
+++ db/seeds/dev/favorites.js
@@ -1,16 +1,34 @@
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('favorites').del()
-      // Inserts seed entries
+  return knex('playlists_favorites').del()
+  .then(() => knex('playlists').del())
+  .then(() => knex('favorites').del())
     .then(() => {
       return Promise.all([
-        knex('favorites').insert({
+        knex('favorites').insert([{
           name: 'Crazy',
           artist_name: 'Patsy Cline',
           genre: 'Country',
           rating: 98
-        }, 'id')
+        }, {
+          name: 'Soy Anormal',
+          artist_name: 'Residente',
+          genre: 'Rap',
+          rating: 100
+        }
+      ], 'id')
+        .then((favorites) => {
+          return knex('playlists').insert([
+            { name: 'Cleaning House' },
+            { name: 'Party Time' }
+          ], 'id')
+          .then((playlists) => {
+            return knex('playlists_favorites').insert([
+              { playlist_id: playlists[0], favorite_id: favorites[0]},
+              { playlist_id: playlists[1], favorite_id: favorites[0]}
+            ])
+          })
+        })
         .then(() => console.log('Seeding complete!'))
         .catch(error => console.log(`Error seeding data: ${error}`))
       ])
