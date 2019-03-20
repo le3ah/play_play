d4ca14d9077e878216b5304917bc61867bedb8a3
Leah Miller <lkmiller.87@gmail.com>
2019-03-20 14:24:22 -0600

    Adds test setup for clearing & seeding database & post fave req.

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -9,6 +9,30 @@ const database = require('../index').database;
 chai.use(chaiHttp);
 
 describe('Favorite Routes', () => {
+  before((done) => {
+    database.raw("TRUNCATE playlists_favorites restart identity;")
+    .then(() => database.raw("TRUNCATE playlists restart identity CASCADE;"))
+    .then(() => database.raw("TRUNCATE favorites restart identity CASCADE;"))
+    .then(() => done())
+    .catch(error => {
+      throw error;
+    });
+  });
+  before((done) => {
+     database.migrate.latest()
+       .then(() => done())
+       .catch(error => {
+         throw error;
+       });
+   });
+
+   beforeEach((done) => {
+     database.seed.run()
+       .then(() => done())
+       .catch(error => {
+         throw error;
+       });
+   });
   it('should return all favorited songs', done => {
     chai.request(server)
     .get('/api/v1/favorites')
@@ -25,6 +49,24 @@ describe('Favorite Routes', () => {
       response.body[0].genre.should.equal('Country');
       response.body[0].should.have.property('rating');
       response.body[0].rating.should.equal(98);
+      done();
+    })
+  })
+});
+describe('POST /api/v1/favorites', () => {
+  it('should create a new favorite', done => {
+    chai.request(server)
+    .post('/api/v1/favorites')
+    .send({
+      name: 'Five Dollar Bill',
+      artist_name: 'Corb Lund',
+      genre: 'AgTrag',
+      rating: 99
+    })
+    .end((err, response) => {
+      response.should.have.status(201);
+      response.body.should.be.a('object');
+      response.body.should.have.property('id');
       done();
     })
   })
