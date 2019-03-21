eb879bcb200d2bfb7b2ac4baaf2ab5d5e9a01296
Mary Goodhart <mary.loves.pry@gmail.com>
2019-03-21 08:22:18 -0600

    Adds heroku configuration

--- knexfile.js
+++ knexfile.js
@@ -31,19 +31,17 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+    client: 'pg',
+    connection: 'postgres://lozlxarvmsdfrj:b38aba55a3300efca3a81d291011b19c69e75622cd7b4048c1fefa9d5bc32da4@ec2-54-83-61-142.compute-1.amazonaws.com:5432/d42t5r4dooak7i',
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './db/migrations'
     }
+    userNullAsDefault: true
   }
 
 };
