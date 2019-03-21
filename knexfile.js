1b07a39727a662ec4a295eb1e993b0c27b0310be
Mary Goodhart <mary.loves.pry@gmail.com>
2019-03-21 08:37:23 -0600

    Updates knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -33,7 +33,6 @@ module.exports = {
   production: {
     client: 'pg',
     connection: 'postgres://lozlxarvmsdfrj:b38aba55a3300efca3a81d291011b19c69e75622cd7b4048c1fefa9d5bc32da4@ec2-54-83-61-142.compute-1.amazonaws.com:5432/d42t5r4dooak7i',
-    },
     pool: {
       min: 2,
       max: 10
@@ -43,4 +42,5 @@ module.exports = {
     },
     useNullAsDefault: true
   }
+
 }
