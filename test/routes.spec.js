0c625f48c741719f82572f65b4a23314cadad59a
Mary Goodhart <mary.loves.pry@gmail.com>
2019-03-25 14:28:17 -0600

    Begins TDD with playlist endpoints

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -124,3 +124,50 @@ describe('DELETE /api/v1/favorites/:id', () => {
     })
   })
 });
+describe('Playlist GET Routes', () => {
+  before((done) => {
+    database.raw("TRUNCATE playlists_favorites restart identity;")
+    .then(() => database.raw("TRUNCATE playlists restart identity CASCADE;"))
+    .then(() => database.raw("TRUNCATE favorites restart identity CASCADE;"))
+    .then(() => done())
+    .catch(error => {
+      throw error;
+    });
+  });
+  before((done) => {
+     database.migrate.latest()
+       .then(() => done())
+       .catch(error => {
+         throw error;
+       });
+   });
+
+   beforeEach((done) => {
+     database.seed.run()
+       .then(() => done())
+       .catch(error => {
+         throw error;
+       });
+   });
+  it('should return all playlists with associated favorites', done => {
+    chai.request(server)
+    .get('/api/v1/playlists')
+    .end((err, response) => {
+      response.should.have.status(200);
+      response.should.be.json;
+      response.body.should.be.a('array');
+      response.body.length.should.equal(2);
+      response.body[0].should.have.property('id');
+      response.body[0].should.have.property('name');
+      done();
+    })
+  })
+  it('should return a 404 for a route that does not exist', done => {
+  chai.request(server)
+    .get('/sad')
+    .end((err, response) => {
+      response.should.have.status(404);
+      done();
+    });
+  });
+});
