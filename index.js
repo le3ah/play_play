01d680d2b8298bd58315d1d27ffb18d3c27d28d4
Leah Miller <lkmiller.87@gmail.com>
2019-03-25 13:32:16 -0600

    Removes post request from index.js file

--- index.js
+++ index.js
@@ -23,31 +23,6 @@ app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
 
-app.post('/api/v1/favorites', (request, response) => {
-  const favorite = request.body;
-  for (let requiredParameter of ['name', 'artist_name', 'genre', 'rating']) {
-    if (!favorite[requiredParameter]) {
-      return response
-      .status(422)
-      .send({ error: `Expected format: { name: <String>, artist_name: <String>, genre: <String>, rating: <Integer> }. You're missing a "${requiredParameter}" property.`});
-    }
-  }
-
-  database('favorites').insert(favorite, ['id', 'name', 'artist_name', 'genre', 'rating'])
-    .then(favorite => {
-      response.status(201).json({
-        id: favorite[0].id,
-        name: favorite[0].name,
-        artist_name: favorite[0].artist_name,
-        genre: favorite[0].genre,
-        rating: favorite[0].rating
-      })
-    })
-    .catch(error => {
-      response.status(500).json({ error });
-    });
-});
-
 app.delete('/api/v1/favorites/:id', (request, response) => {
   database('favorites').where({id: request.params.id }).del()
   .then(favoriteId => {
